24314a64bd02356da7257113604726a0b84c072e
lucasborgesdasilva <lucasborges2812@gmail.com>
2024-10-03 15:33:01 -0300

    Criado componente Sidebar e adicionada lib phosphorIcons

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,35 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './styles.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src='https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+        alt='banner profile'
+      />
+
+      <div className={styles.profile}>
+        <img 
+          className={styles.avatar} 
+          src="https://github.com/lucasborgesdasilva.png" 
+          alt="" 
+        />
+
+        <strong>Lucas Borges</strong>
+        <span>FullStack Developer</span>
+      </div>
+
+      <footer>
+        <a 
+          href="https://www.google.com" 
+          target='_blank' rel="noreferrer"
+        >
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
