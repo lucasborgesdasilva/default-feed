7cfb4dc3b0966861b5b668c0734507f83e576e40
lucasborgesdasilva <lucasborges2812@gmail.com>
2024-10-04 21:55:56 -0300

    Ajustado componente de Post com o form de comentário

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -35,6 +35,18 @@ export function Post() {
           <a href="https://www.google.com">#rocketseat</a>{' '}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button> 
+        </footer>
+      </form>
     </article>
   )
 }
