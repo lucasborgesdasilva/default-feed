a142a0525536f9001ef6019b7208da1905cf8aeb
lucasborgesdasilva <lucasborges2812@gmail.com>
2024-10-04 22:49:57 -0300

    Criado component de Comentários e outros ajustes

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './styles.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/lucasborgesdasilva.png" alt="" />
+      
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Borges</strong>
+              <time 
+                title='03 de Outubro as 16:32h'
+                dateTime='2024-10-03 16:32:00'
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button type='button' title='Deletar comentário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button type='button'>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+
+
+    </div>
+  )
+}
